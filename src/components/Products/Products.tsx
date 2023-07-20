c2eec2cacbcc96ab175eca377f5b8872559e32dc
Bishwo Bista <bishwo5bista@gmail.com>
2023-07-20 22:18:22 +0545

    fix: Link position

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -37,9 +37,10 @@ const Products = () => {
           products.map(
             (product: any) => {
               return (
-                <Link to={`/product/${product.id}`} key={product.id}>
+                
                   <div className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
                   key={product.id}>
+                    <Link to={`/product/${product.id}`} key={product.id}>
                   <h2
                     className='text-gray-700 text-base'
                   >{product.title}</h2>
@@ -47,6 +48,7 @@ const Products = () => {
                   <p
                     className='text-gray-700 text-base'
                   >{product.price}</p>
+                  </Link>
                   <button
                     className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
                     onClick={
@@ -58,7 +60,7 @@ const Products = () => {
                     Add to cart
                   </button>
                 </div>
-                </Link>
+                
               )
             }
           )
