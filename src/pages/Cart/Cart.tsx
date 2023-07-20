4272f81a634ff5731163772c28f6faa1615535a2
Bishwo Bista <bishwo5bista@gmail.com>
2023-07-20 18:04:33 +0545

    update cart slice

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import { useDispatch, useSelector } from "react-redux"
+import { remove } from "../../store/CartSlice/CartSlice"
+
 
 const Cart = () => {
+  const products = useSelector((state: any) => state.cart)
+
+  const dispatch = useDispatch()
+
+  const handleClick = (id: number) => () => {
+    dispatch(remove(id))
+  }
+  
   return (
-    <div>Cart</div>
+    <>
+      <h1
+      className="text-2xl font-bold"
+      >Cart</h1>
+      <div
+      className="flex flex-col space-y-4"
+      >
+        {
+          products.map((product: any) => {
+            return (
+              <div key={product.id}
+              className="flex justify-between items-center"
+              >
+                <h3
+                className="text-xl font-bold"
+                >{product.title}</h3>
+
+                <p
+                className="text-xl font-bold"
+                >{product.price}</p>
+
+                <button
+                className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+                onClick={handleClick(product.id)}
+                >Remove</button>
+              </div>
+            )
+          })
+        }
+      </div>
+    </>
   )
 }
 
--- src/store/CartSlice/CartSlice.tsx
+++ src/store/CartSlice/CartSlice.tsx
@@ -13,7 +13,7 @@ const cartSlice = createSlice({
       state.push(action.payload);
     },
     remove(state: CartItem[], action: { payload: number }) {
-      state = state.filter(item => item.id !== action.payload);
+      return state.filter(item => item.id !== action.payload);
       
     },
   },
