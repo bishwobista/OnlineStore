cc28ac41e587ea4de7e53cdad32b1e81fc32383e
Bishwo Bista <bishwo5bista@gmail.com>
2023-07-20 18:05:43 +0545

    update cart slice, add product slice for category

---
+++ src/store/productSlice/productSlice.tsx
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+interface CartItem {
+  id: number; 
+}
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: [] as CartItem[], 
+  reducers: {
+    add(state: CartItem[], action: { payload: CartItem }) {
+      state.push(action.payload);
+    },
+    remove(state: CartItem[], action: { payload: number }) {
+      return state.filter(item => item.id !== action.payload);
+      
+    },
+  },
+});
+
+export const { add, remove } = cartSlice.actions;
+
+export default cartSlice.reducer;
