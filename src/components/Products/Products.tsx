f609fa8edfb488994a758a378275f204ea4e2b9b
Bishwo Bista <bishwo5bista@gmail.com>
2023-07-20 20:03:24 +0545

    implemented react query to fetch data

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,58 +1,75 @@
-import React, { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { add } from '../../store/CartSlice/CartSlice'
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
+
+// interface Product {
+//   id: number,
+//   title: string,
+//   price: number,
+//   description: string,
+//   category: string,
+//   image: string
+// }
 const Products = () => {
   const dispatch = useDispatch();
-    const [products, setProducts] = useState([])
-  useEffect(() => {
-    
-    const fetchProducts = async () => {
-      const response = await fetch('http://fakestoreapi.com/products')
-      const data = await response.json()
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
 
   const handleClick = (product: any) => {
     dispatch(add(product));
   }
+  const [products, setProducts] = useState([])
+
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      axios.get('https://fakestoreapi.com/products').then((res) => res.data),
+    onSuccess: (data) => setProducts(data),
+
+  })
+  
+
+  if (isLoading) return 'Loading...'
+
+  if (error) return 'An error has occurred: '
 
   return (
     <>
-        <h1>Products</h1>
-        <div className='grid grid-cols-4 gap-4'>
+      <h1>Products</h1>
+      <div className='grid grid-cols-4 gap-4'>
         {
-            products.map(
-                (product: any) => {
-                    return (
-                        <div className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
-                         key={product.id}>
-                            <h2
-                            className='text-gray-700 text-base'
-                            >{product.title}</h2>
-                            <img className="object-contain h-48 w-96" src={product.image} alt={product.title} />
-                            <p
-                            className='text-gray-700 text-base'
-                            >{product.price}</p>
-                            <button 
-                            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-                            onClick={
-                              () =>{
-                                handleClick(product)
-                              }
-                            }
-                            >
-                                Add to cart
-                            </button>
-                        </div>
-                    )
-                }
-            )
+          products.map(
+            (product: any) => {
+              return (
+                <div className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
+                  key={product.id}>
+                  <h2
+                    className='text-gray-700 text-base'
+                  >{product.title}</h2>
+                  <img className="object-contain h-48 w-96" src={product.image} alt={product.title} />
+                  <p
+                    className='text-gray-700 text-base'
+                  >{product.price}</p>
+                  <button
+                    className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+                    onClick={
+                      () => {
+                        handleClick(product)
+                      }
+                    }
+                  >
+                    Add to cart
+                  </button>
+                </div>
+              )
+            }
+          )
         }
-        </div>
+      </div>
     </>
+
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,24 @@ import './index.css'
 import { Provider } from 'react-redux'
 import Store  from './store/Store/Store'
 
+import {
+  
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <Provider store={Store}>
+    <QueryClientProvider client={queryClient}>
+    <Provider store={Store}>
       <App />
     </Provider>
+    </QueryClientProvider>
+    
   </React.StrictMode>,
 )
+
+
+
