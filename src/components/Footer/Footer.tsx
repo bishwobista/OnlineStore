a8dffa5d3949432f97f674cb23d61265367ce906
Bishwo Bista <bishwo5bista@gmail.com>
2023-07-21 15:05:15 +0545

    change footer color

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
   
-  <footer className="bg-slate-400 py-12 ">
+  <footer className="bg-stone-200 py-12 ">
     <div className="container mx-auto">
       <p className="ext-white text-center">Copyright &copy; Online Store 2023. All Rights Reserved. </p>
     </div>
