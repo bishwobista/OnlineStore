0277604375d63e0610cc31799a8d44c05088f4b9
Bishwo Bista <bishwo5bista@gmail.com>
2023-07-21 10:50:12 +0545

    nav position fix

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,6 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import { AiOutlineShoppingCart } from 'react-icons/ai'
 import { useSelector } from "react-redux";
 
-
 const Navbar = () => {
   //responsive menu using react-icons
 
@@ -12,66 +11,35 @@ const Navbar = () => {
   });
 
   return (
-
-    <>
-    <header
-    className="i py-4 px-8 bg-white shadow-md"
-    >
-      <nav 
-      className="flex justify-between items-center"
-      >
-        <a href="/"
-        className="text-2xl font-bold text-gray-800 lg:text-3xl hover:text-gray-700"
-        >
-          Online Store
-        </a>
-        <ul
-        className=" lg:flex lg:flex-row lg:space-x-8"
-        >
-          <li
-          className="text-gray-700 font-medium text-base hover:text-gray-600"
-          >
-            <NavLink
-              to="/"
-            
-            >
-              Home
-            </NavLink>
-          </li>
-          <li
-          className="text-gray-700 font-medium text-base hover:text-gray-600"
-          >
-            <NavLink
-              to="/cart"
-            
-            >
-              <AiOutlineShoppingCart className="text-xl mr-2" />
-              
-            </NavLink>
-          </li>
-          <li
-          className="text-gray-700 font-medium text-base hover:text-gray-600"
-          >
-            <span
-            className="flex items-center"
-            >{items.length} </span>
-          </li>
-          <li
-          className="text-gray-700 font-medium text-base hover:text-gray-600"
-          >
-            <NavLink
-              to="/search"
-              >
-                <input 
-            className="border border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-            type="search" placeholder="Search..." />
+    <header className="text-gray-600 body-font">
+      <nav className="flex justify-between items-center">
+        <div className="flex items-center space-x-4">
+          <NavLink to="/" className="text-2xl font-bold text-gray-800 lg:text-3xl hover:text-gray-700">
+            Online Store
+          </NavLink>
+          <ul className="lg:flex lg:flex-row lg:space-x-8">
+            <li className="text-gray-700 font-medium text-base hover:text-gray-600">
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li className="text-gray-700 font-medium text-base hover:text-gray-600">
+              <NavLink to="/search">
+                Search
               </NavLink>
-          </li>
-        </ul>
+            </li>
+          </ul>
+        </div>
+        <div className="flex items-center space-x-4">
+          <NavLink to="/cart">
+            <span className="relative inline-block">
+              <AiOutlineShoppingCart className="text-xl" />
+              {items.length > 0 && (
+                <sup className="absolute top-0 right-0 bg-red-500 text-white rounded-full px-1 text-xs">{items.length}</sup>
+              )}
+            </span>
+          </NavLink>
+        </div>
       </nav>
     </header>
-    
-    </>
   );
 };
 
