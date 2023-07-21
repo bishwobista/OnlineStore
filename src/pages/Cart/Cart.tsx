175136e85cb64e32931eb8b72c786696f7d5aca1
Bishwo Bista <bishwo5bista@gmail.com>
2023-07-21 18:29:50 +0545

    cart slice working

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux"
 import { remove } from "../../store/CartSlice/CartSlice"
-
+import { IoMdClose, IoMdRemove, IoMdAdd } from "react-icons/io"
+import { parse } from "dotenv"
 
 const Cart = () => {
   const products = useSelector((state: any) => state.cart)
@@ -10,34 +11,57 @@ const Cart = () => {
   const handleClick = (id: number) => () => {
     dispatch(remove(id))
   }
-  
+
+  const amount = 1
   return (
     <>
-      <h1
-      className="text-2xl font-bold"
-      >Cart</h1>
-      <div
-      className="flex flex-col space-y-4"
-      >
+      
+      <div className="pt-[80px] mx-h-full h-[calc(100vh-80px)] overflow-y-auto">
+      
         {
           products.map((product: any) => {
             return (
-              <div key={product.id}
-              className="flex justify-between items-center"
-              >
-                <h3
-                className="text-xl font-bold"
-                >{product.title}</h3>
-
-                <p
-                className="text-xl font-bold"
-                >{product.price}</p>
-
-                <button
-                className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
-                onClick={handleClick(product.id)}
-                >Remove</button>
-              </div>
+              <section className="flex  py-2 lg:px-6 border-b border-gray-200 w-full  font-light text-gary-500 ">
+                <div className="w-full max-w-full  min-h-[150px] flex items-center gap-x-4">
+                  <div>
+                    <img src={product.image} className="max-w-[80px]" />
+                  </div>
+                  <div className="w-[50%] flex flex-col">
+                    <div className="flex justify-evenly md-2">
+                      <div className="text-sm uppercase font-medium max-w-[240px] hover:underline">
+                        {product.title}
+                      </div>
+                      <div className="text-xl cursor-pointer ">
+                      <button
+                            onClick={handleClick(product.id)}
+                            >
+                        <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
+                            </button>
+                      </div>
+                      <div className="flex gap-x-2 h-[36px] text-sm">
+                        <div className="flex flex-1 max-w-[100px] items-center h-full border font-medium">
+                          <div className="flex-1 h-full flex justify-center items-center cursor-pointer">
+                            
+                            <IoMdRemove />
+                          </div>
+                          <div className="h-full flex justify-center items-center px-2">
+                            {amount}
+                          </div>
+                          <div className="flex-1 h-full flex justify-center items-center cursor-pointer">
+                            <IoMdAdd />
+                          </div>
+                        </div>
+                        <div className="flex-1 flex items-center justify-around">
+                          {product.price}
+                        </div>
+                        <div className="flex-1 flex justify-end items-center font-medium">
+                        ${parseFloat(product.price).toFixed(2)}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
             )
           })
         }
