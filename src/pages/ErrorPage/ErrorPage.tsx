aa9e028e0060ff59f02b1d2f6032f1da0b0f0d80
Bishwo Bista <bishwo5bista@gmail.com>
2023-07-21 20:36:53 +0545

    create error page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import  { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ErrorPage = () => {
-  return (
-    <div>ErrorPage</div>
-  )
-}
+  const navigate = useNavigate();
+  useEffect(() => {
+    
+    const redirectTimeout = setTimeout(() => {
+      navigate('/');
+    }, 3000);
 
-export default ErrorPage
+    return () => clearTimeout(redirectTimeout);
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-red-500">404 Not Found</h1>
+        <p className="mt-4 text-gray-600">The page you're looking for does not exist.</p>
+        <p className="mt-4 text-gray-600">You will be redirected to the homepage shortly.</p>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
